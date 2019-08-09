03d0c3930d8f82356029911f93c13f4baa0624a0
Elizabeth Kelen <erskelen@gmail.com>
2019-08-09 14:03:31 +0200

    test push

--- pruning/minimax_test.go
+++ pruning/minimax_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestLaunchMinimax(t *testing.T) {
 	/*
-	 **
+	 ***
 	 **                                1
 	 **                ---------------------------------
 	 **                2                               3
